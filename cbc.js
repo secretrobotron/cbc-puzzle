4ee3da5a7a3c6e0c9a1830cce63e48dadec89cfc
David Seifried <david.c.seifried@gmail.com>
2011-10-03 17:23:59 -0400

    First working version of the puzzle stuff

--- cbc.js
+++ cbc.js
@@ -1,5 +1,6 @@
+(function() {
 //  Thanks to Paul Irish for this code
-window.requestAnimFrame = (function(){
+var requestAnimFrame = (function(){
   return  window.requestAnimationFrame       || 
           window.webkitRequestAnimationFrame || 
           window.mozRequestAnimationFrame    || 
@@ -10,18 +11,93 @@ window.requestAnimFrame = (function(){
             };
 })();
 
-var Cbc = function( target, timeline, itemBox ) {
+var removeClass = function( element, name ) {
+  var classes = element.className.split( " " ),
+      idx = classes.indexOf( name );
+  if ( idx ) {
+    classes.splice( idx, 1 );
+  }
+  element.className = classes.join( " " );
+};
+
+var addClass = function( element, name ) {
+
+  var classes = element.className.split( " " ),
+      idx = classes.indexOf( name );
+  if ( idx === -1 ) {
+    element.className += " " + name;
+  }
+};
+
+var Cbc = window.Cbc = function( target, itemBox, timeline ) {
 
   var audioElement = this.audioElement = document.getElementById( target ),
       timelineElement = this.timelineElement = document.getElementById( timeline ),
       itemBoxElement = this.itemBoxlLement= document.getElementById( itemBox ),
-      events = this.events = [];
+      playBtn = document.getElementById( "play" ),
+      events = this.events = [],
+      playingEvents = this.playingEvents = {},
+      playingIndex = 0,
+      stopSpinnin = false;
 
-  timelineElement.addEventListener( "drop", function( event ) {
+  var ridinSpinnas = function( options, cb ) {
+
+    if ( audioElement.currentTime >= options.end ) {
+      cb && cb();
+    } else if ( !stopSpinnin ){
+      requestAnimFrame( function(){
+        ridinSpinnas( options, cb );
+      });
+    }
+  };
+
+  var getNextChild = function( children ) {
+  
+    var itemsLength = children.length;
+    var options;
+    if ( children( playingIndex ).children.length > 0 ) {
+      options = playingEvents[ children.item( playingIndex ).children.item( 0 ).id ];
+      audioElement.currentTime = options.start;
+      audioElement.play();
+    }
+    
+    if ( options ) {
+      requestAnimFrame( function() {
+        ridinSpinnas( options, function() {
+          playingIndex++;
+          if ( playingIndex >= itemsLength ) {
+            playingIndex = 0;
+            audioElement.pause();
+          } else { 
+            getNextChild( children );
+          }
+        });
+      });
+    } else {
+      playingIndex++;
+      if ( playingIndex === itemsLength ) {
+        audioElement.pause();
+        playingIndex = 0;
+      } else { 
+        getNextChild( children );
+      }
+    }
+  };
+
+  playBtn.addEventListener( "click", function( event ) {
+    stopSpinnin = false;
+    for ( var i = 0; i < events.length; i++ ) {
+      events[ i ].stop();
+    }
+    playingIndex = 0;
+    var children = timelineElement.children; 
+    getNextChild( children );
+  }, false );
+
+  itemBoxElement.addEventListener( "drop", function( event ) {
 
    event.stopPropagation && event.stopPropagation();
    var newItem = document.getElementById( event.dataTransfer.getData( "Text" ) );
-   newItem.style.left = "20px";
    this.appendChild( newItem );
   }, false );
 
@@ -35,19 +111,23 @@ var Cbc = function( target, timeline, itemBox ) {
 
   this.registerEvent = function( options ) {
 
+    options.stop = function() {
+      options.pauseMe = false;
+      audioElement.pause();
+    };
+  
     var animloop = function() {
-
+      
       if ( options.pauseMe && audioElement.currentTime >= options.end ) {
-        options.pauseMe = false;
-        audioElement.pause();
+        options.stop();
       } else if ( options.pauseMe ) {
         requestAnimFrame( animloop );
       }
     };
 
-    var target = document.getElementById( options.target ),
+    var target = itemBoxElement,
         eventDiv = document.createElement( "div" ),
-        innerDiv = document.createElement( "span" );
+        innerDiv = document.createElement( "div" );
 
     options.eventDiv = eventDiv;
 
@@ -56,27 +136,39 @@ var Cbc = function( target, timeline, itemBox ) {
     eventDiv.id = "cbc-event-" + this.counter++;
     eventDiv.innerHTML = options.text;
 
-    innerDiv.style.width = "65.5px";
-    innerDiv.style.height = "44px";
+    playingEvents[ eventDiv.id ] = options;
+
+    innerDiv.style.float = "left";
+    addClass( innerDiv, "cbc-puzzle-event" );
+    addClass( innerDiv, "cbc-puzzle-unhighlight" );
 
 
     innerDiv.addEventListener( "dragover", function( event ) {
 
+      addClass( this, "cbc-puzzle-highlight" ); 
       event.preventDefault && event.preventDefault();
       event.dataTransfer.dropeffect = "copy";
     }, false );
 
+    innerDiv.addEventListener( "dragleave", function( event ) {
+  
+      removeClass( this, "cbc-puzzle-highlight" );
+    }, false);
 
-  innerDiv.addEventListener( "drop", function( event ) {
 
-   event.stopPropagation && event.stopPropagation();
+    innerDiv.addEventListener( "drop", function( event ) {
 
-   var newItem = document.getElementById( event.dataTransfer.getData( "Text" ) );
-   newItem.style.left = "20px";
-   this.appendChild( newItem );
-  }, false );
+      removeClass( this, "cbc-puzzle-highlight" );
+      event.stopPropagation && event.stopPropagation();
+      if ( this.innerHTML === "" ) {
 
-    itemBoxElement.appendChild( innerDiv ); 
+        var newItem = document.getElementById( event.dataTransfer.getData( "Text" ) );
+        this.style.width = newItem.style.width;
+        this.appendChild( newItem );
+      }
+    }, false );
+
+    timelineElement.appendChild( innerDiv ); 
 
     eventDiv.addEventListener( "click", function( event ) {
 
@@ -86,6 +178,7 @@ var Cbc = function( target, timeline, itemBox ) {
         events[ i ].pauseMe = false;
       }
 
+      stopSpinnin = true;
       options.pauseMe = true;
       animloop();
       audioElement.play();
@@ -94,6 +187,7 @@ var Cbc = function( target, timeline, itemBox ) {
     eventDiv.draggable = true;
 
     eventDiv.addEventListener( "dragstart", function( event ) {
+      
       event.dataTransfer.effectAllowed = "copy";
       event.dataTransfer.setData( "Text", this.id );
     }, false );
@@ -102,3 +196,4 @@ var Cbc = function( target, timeline, itemBox ) {
 
   }
 }
+})();
