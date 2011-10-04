cf86e13dee1f14e95f7a75004d2505fbedc2fb91
David Seifried <david.c.seifried@gmail.com>
2011-10-04 12:54:47 -0400

    Added in end condition, making items undragable when they are in the correct position

--- cbc.js
+++ cbc.js
@@ -6,12 +6,13 @@ var requestAnimFrame = (function(){
           window.mozRequestAnimationFrame    || 
           window.oRequestAnimationFrame      || 
           window.msRequestAnimationFrame     || 
-            function(/* function */ callback, /* DOMElement */ element){
-              window.setTimeout(callback, 1000 / 60);
-            };
+          function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout( callback, 1000 / 60);
+          };
 })();
 
 var removeClass = function( element, name ) {
+
   var classes = element.className.split( " " ),
       idx = classes.indexOf( name );
   if ( idx ) {
@@ -29,6 +30,10 @@ var addClass = function( element, name ) {
   }
 };
 
+var winner = function() {
+  console.log( "we have won" );
+}
+
 var Cbc = window.Cbc = function( target, itemBox, timeline ) {
 
   var audioElement = this.audioElement = document.getElementById( target ),
@@ -36,15 +41,17 @@ var Cbc = window.Cbc = function( target, itemBox, timeline ) {
       itemBoxElement = this.itemBoxlLement= document.getElementById( itemBox ),
       playBtn = document.getElementById( "play" ),
       events = this.events = [],
+      sortedEvents = [],
       playingEvents = this.playingEvents = {},
       playingIndex = 0,
-      stopSpinnin = false;
+      stopSpinnin = false,
+      lastEnd;
 
   var ridinSpinnas = function( options, cb ) {
 
     if ( audioElement.currentTime >= options.end ) {
       cb && cb();
-    } else if ( !stopSpinnin ){
+    } else if ( !stopSpinnin ) {
       requestAnimFrame( function(){
         ridinSpinnas( options, cb );
       });
@@ -53,51 +60,82 @@ var Cbc = window.Cbc = function( target, itemBox, timeline ) {
 
   var getNextChild = function( children ) {
   
-    var itemsLength = children.length;
-    var options;
-    if ( children( playingIndex ).children.length > 0 ) {
-      options = playingEvents[ children.item( playingIndex ).children.item( 0 ).id ];
-      audioElement.currentTime = options.start;
-      audioElement.play();
-      addClass( children.item( playingIndex ), "cbc-puzzle-playing" );
-    }
-    
-    if ( options ) {
-      requestAnimFrame( function() {
-        ridinSpinnas( options, function() {   
-          removeClass( children.item( playingIndex ), "cbc-puzzle-playing" );
-          playingIndex++;
-          if ( playingIndex >= itemsLength ) {
-            playingIndex = 0;
-            audioElement.pause();
-          } else { 
-            getNextChild( children );
-          }
-        });
-      });
-    } else {
+    var increment = function() {
+
       playingIndex++;
-      if ( playingIndex === itemsLength ) {
+      if ( playingIndex >= itemsLength ) { 
         audioElement.pause();
         playingIndex = 0;
       } else { 
         getNextChild( children );
       }
+    },
+
+    itemsLength = children.length,
+    childsChildren = children[ playingIndex ].children,
+    currentNode = children[ playingIndex ],
+    options;
+
+    if ( childsChildren.length > 0 ) { 
+
+      options = playingEvents[ childsChildren.item( 0 ).id ];
+
+      if ( options.start !== lastEnd ) {
+        audioElement.currentTime = options.start;
+      }
+
+      lastEnd = options.end;
+      audioElement.play();
+      addClass( currentNode, "cbc-puzzle-playing" );
+    }
+    
+    if ( options ) {
+
+      requestAnimFrame( function() {
+        ridinSpinnas( options, function() {   
+          removeClass( currentNode, "cbc-puzzle-playing" );
+          increment();
+        });
+      });
+    } else {
+      increment();
     }
   };
 
   playBtn.addEventListener( "click", function( event ) {
-    stopSpinnin = false;
+    
+    var children = timelineElement.children,
+        correctItems = 0, 
+        stopSpinnin = false,
+        childsChild;
+
+    sortedEvents.sort();
+
     for ( var i = 0; i < events.length; i++ ) {
+
+      childsChild = children[ i ].children.item( 0 ).id;
+
+      if ( children[ i ].children.length && playingEvents[ childsChild ].start === sortedEvents[ i ] ) {
+        correctItems++;
+        var cbcEvent = document.getElementById( childsChild );
+        addClass( cbcEvent, "cbc-puzzle-correctNode" );
+        cbcEvent.draggable = false;
+      }
+
       events[ i ].stop();
     }
+
+    if ( correctItems === children.length ) {
+      winner();
+    } 
+
     playingIndex = 0;
-    var children = timelineElement.children; 
     getNextChild( children );
   }, false );
 
   itemBoxElement.addEventListener( "drop", function( event ) {
-
+   
+   event.preventDefault && event.preventDefault();
    event.stopPropagation && event.stopPropagation();
    var newItem = document.getElementById( event.dataTransfer.getData( "Text" ) );
    this.appendChild( newItem );
@@ -134,6 +172,7 @@ var Cbc = window.Cbc = function( target, itemBox, timeline ) {
     options.eventDiv = eventDiv;
 
     this.events.push( options );
+    sortedEvents.push( options.start );
 
     eventDiv.id = "cbc-event-" + this.counter++;
     eventDiv.innerHTML = options.text;
@@ -160,6 +199,7 @@ var Cbc = window.Cbc = function( target, itemBox, timeline ) {
 
     innerDiv.addEventListener( "drop", function( event ) {
 
+      event.preventDefault && event.preventDefault();
       removeClass( this, "cbc-puzzle-highlight" );
       event.stopPropagation && event.stopPropagation();
       if ( this.innerHTML === "" ) {
@@ -195,7 +235,6 @@ var Cbc = window.Cbc = function( target, itemBox, timeline ) {
     }, false );
 
     target.appendChild( eventDiv );
-
   }
 }
 })();
